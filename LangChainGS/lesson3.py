45e5fba3e2a44525cb3c6bf295c55173c53bd9eb
Nick Swekosky <nick.swekosky@gmail.com>
2023-04-10 18:50:44 +0000

    Added first 3 lessons for LangChain GS tutorial

---
+++ LangChainGS/lesson3.py
@@ -0,0 +1,14 @@
+from langchain.prompts import PromptTemplate
+from langchain.llms import OpenAI
+
+llm = OpenAI(temperature=0.9)
+prompt = PromptTemplate(
+    input_variables=["product"],
+    template="What is a good name for a company that makes {product}?",
+)
+
+from langchain.chains import LLMChain
+chain = LLMChain(llm=llm, prompt=prompt)
+
+print(chain.run("colorful socks"))
+# -> '\n\nSocktastic!'
